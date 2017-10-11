06090ab1e640c9cdb880a13f3ea61819216eda66
atacanjacques <atacan.jacques@gmail.com>
2017-10-11 12:14:00 +0200

    added resa function with date resolve

--- app.js
+++ app.js
@@ -23,18 +23,77 @@ var bot = new builder.UniversalBot(connector, [
 
 bot.dialog('greetings', [
     function (session) {
+        session.send('Bienvenue dans le bot Résa');
         session.beginDialog('askName');
     },
     function (session, results) {
-        session.endDialog('Hello %s!', results.response);
+        session.userData.userName = results.response;
+        session.send('Bonjour %s!', session.userData.userName);
+        session.beginDialog('resa');
     }
 ]);
 
 bot.dialog('askName', [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        builder.Prompts.text(session, 'Quel est votre nom ?');
     },
     function (session, results) {
         session.endDialogWithResult(results);
+    }
+]);
+
+bot.dialog('resa', [
+    function (session) {
+        session.beginDialog('resaDate');
+    },
+    function (session, results) {
+        session.beginDialog('resaNumber');
+    },
+    function (session) {
+        session.beginDialog('resaName');
+    },
+    function (session) {
+        var resa = {
+            resaDate: session.conversationData.resaDate,
+            resaNb: session.conversationData.resaNb,
+            resaName: session.conversationData.resaName
+        }
+
+        resa.resaDate = new Date(Date.parse(resa.resaDate));
+        resa.resaDate = resa.resaDate.toDateString();
+
+        session.send('Date: ' + resa.resaDate);
+        session.send('Nombre de personne: ' + resa.resaNb);
+        session.send('Résa au nom de: ' + resa.resaName);
+    }
+]);
+
+bot.dialog('resaDate', [
+    function (session) {
+        builder.Prompts.time(session, "Pour quand voulez vous résérvez ?");
+    },
+    function (session, results) {
+        session.conversationData.resaDate = builder.EntityRecognizer.resolveTime([results.response]);
+        session.endDialog();
+    }
+]);
+
+bot.dialog('resaNumber', [
+    function (session) {
+        builder.Prompts.number(session, "Combien de personne ?");
+    },
+    function (session, results) {
+        session.conversationData.resaNb = results.response;
+        session.endDialog();
+    }
+]);
+
+bot.dialog('resaName', [
+    function (session) {
+        builder.Prompts.text(session, "Résa au nom de qui ?");
+    },
+    function (session, results) {
+        session.conversationData.resaName = results.response;
+        session.endDialog();
     }
 ]);
